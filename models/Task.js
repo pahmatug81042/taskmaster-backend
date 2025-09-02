74644a176c4af5f26d510dbd20b921d380fcb3ef
Parsa Ahmadi Nasab Emran <parsatempleuniversity@outlook.com>
2025-09-02 17:09:19 -0400

    feat(Task): add status enum with default value 'To Do'

--- models/Task.js
+++ models/Task.js
@@ -4,6 +4,11 @@ const taskSchema = new mongoose.Schema(
     {
         title: { type: String, required: [true, 'Task title is required'] },
         description: { type: String },
+        status: {
+            type: String,
+            enum: ['To Do', 'In Progress', 'Done'],
+            default: 'To Do',
+        },
     },
     { timestamps: true }
 );
