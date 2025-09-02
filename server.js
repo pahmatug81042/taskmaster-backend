3f47c8b1bb23713de3e8cc3cf0d596bf7bf20f0d
Parsa Ahmadi Nasab Emran <parsatempleuniversity@outlook.com>
2025-09-02 19:26:56 -0400

    chore(server): import dependencies (express, dotenv, connectDB, cors)

---
+++ server.js
@@ -0,0 +1,6 @@
+// Import the necessary modules
+
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db'); // Import the DB connection
+const cors = require('cors'); // Import CORS middleware
