4ec92db938423516777cea29793286fb3645bb6f
Parsa Ahmadi Nasab Emran <parsatempleuniversity@outlook.com>
2025-09-03 10:26:11 -0400

    feat(projectRoutes): add POST and GET all routes with auth

--- routes/api/projectRoutes.js
+++ routes/api/projectRoutes.js
@@ -8,3 +8,6 @@ const {
 } = require('../../controllers/projectController');
 const auth = require('../../middleware/authMiddleware');
 const router = express.Router();
+
+router.post('/', auth, createProject);
+router.get('/', auth, getProjects);
