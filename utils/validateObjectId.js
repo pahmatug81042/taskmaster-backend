d1a5a4566709c6ccb14b8c4c02ec0206b2fb2850
Parsa Ahmadi Nasab Emran <parsatempleuniversity@outlook.com>
2025-10-03 18:16:46 -0400

    updated the validateObjectId

--- utils/validateObjectId.js
+++ utils/validateObjectId.js
@@ -1,7 +1,17 @@
-const validator = require("validator");
+const mongoose = require("mongoose");
 
-const validateObjectId = (id) => {
-    return validator.isMongoId(id);
+/**
+ * Middleware to validate MongoDB objectId params
+ * Blocks requests early if the ID is malformed
+ */
+const validateObjectId = (paramName) => {
+    return (req, res, next) => {
+        const id = req.params[paramName];
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ message: `Invalid ${paramName}` });
+        }
+        next();
+    };
 };
 
 module.exports = validateObjectId;
